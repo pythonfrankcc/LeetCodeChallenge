a6743169468d766e1cb0f4c1a8b576bd706289fa
pythonfrankcc <francismurithi475@gmail.com>
2022-11-16 04:35:13 +0300

    day 7

--- IndicesThatAddToNumber/IndicesAdd.py
+++ IndicesThatAddToNumber/IndicesAdd.py
@@ -1,13 +1,17 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numtoindexmap = {}
-        for num1_index, num1 in enumerate(nums):
-            num2 = target - num1
-            try:
-                num2_index = numtoindexmap[num2]
-            except KeyError:
-                numtoindexmap[num1] = num1_index
-            else:
-                return tuple(sorted([num1_index, num2_index]))
-                
+def checkSum(lst,target):
+    lst1 = [];
+    lstSize = len(lst)
+    for i in range(lstSize-1):
+        for j in range(i+1, lstSize):
+            if (lst[i] + lst[j] == target):
+                index1 = lst.index(lst[i])
+                #print(index1)
+                index2 = lst.index(lst[j])
+                lst1.append(index1)
+                lst1.append(index2)
+                return sorted(lst1)
+
+    return 0
+a = checkSum([0, -1, 2, -3, 1],-2)
+print(a)
         
